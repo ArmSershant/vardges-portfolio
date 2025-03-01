7d31e15b6fa082bb148890b4058b3c9a8fa4fb99
ArmSershant <vardges.movsesyan6@gmail.com>
2025-03-01 13:23:50 +0400

    Changes in modal

--- src/components/atoms/Modal/Modal.tsx
+++ src/components/atoms/Modal/Modal.tsx
@@ -15,24 +15,32 @@ const Modal: FC<ModalProps> = ({ project, onClose }) => {
         >
           &times;
         </button>
-        {project.videoURL ? (
-          <video
-            src={project.videoURL}
-            controls
-            className={styles.modalOverlay_modal_projectVideo}
-          />
-        ) : (
-          <img
-            src={project.photoURL}
-            alt={project.title}
-            className={styles.modalOverlay_modal_projectImage}
-          />
-        )}
-        <h2 className={styles.modalOverlay_modal_title}>{project.title}</h2>
-        <p className={styles.modalOverlay_modal_type}>Type: {project.type}</p>
-        <p className={styles.modalOverlay_modal_description}>
-          Description: {project.description}
-        </p>
+        <div className={styles.modalOverlay_modal_content}>
+          {project.videoURL ? (
+            <video
+              src={project.videoURL}
+              controls
+              className={styles.modalOverlay_modal_content_projectVideo}
+            />
+          ) : (
+            <img
+              src={project.photoURL}
+              alt={project.title}
+              className={styles.modalOverlay_modal_content_projectImage}
+            />
+          )}
+          <div className={styles.modalOverlay_modal_content_projectDetails}>
+            <h2 className={styles.modalOverlay_modal_projectDetails_title}>
+              {project.title}
+            </h2>
+            <p className={styles.modalOverlay_modal_projectDetails_type}>
+              Type: {project.type}
+            </p>
+            <p className={styles.modalOverlay_modal_projectDetails_description}>
+              Description: {project.description}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
