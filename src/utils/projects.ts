bf45b5bbbf7737821e02f21770a446087bf0fde8
ArmSershant <vardges.movsesyan6@gmail.com>
2025-03-11 16:12:27 +0400

    Updated project logos and improved css

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,3 +1,7 @@
+import archLogo from "../assets/images/projects/arch-games-logo.png";
+import exodusLogo from "../assets/images/projects/exodus-logo.png";
+import triliumGameLogo from "../assets/images/projects/trilium-quest-game-logo.png";
+import triliumWikiLogo from "../assets/images/projects/trilium-quest-web-logo.png";
 import { Project } from "./types/types";
 
 export const projects: Project[] = [
@@ -5,15 +9,15 @@ export const projects: Project[] = [
     title: "Exodus",
     description:
       "Exodus Metaverse is NFT Marketplace on blockchain. Thanks to blockchain technology, you can enjoy secure and transparent transactions, as well as true ownership and control over your NFTs and resources. You can connect one of your wallets Anchor, MetaMask, Terra or Cloud Wallet to do the importing and exporting of NFTs also for the transactions.",
-    photoURL:
-      "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/project_item/attachment/d0dafb0ba6df634b30dc3a7e99097602-1737728742623/image_original.jpeg",
+    photoURL: exodusLogo,
     videoURL:
       "https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/v1/video-attachments/project_item/attachment/b2cfe9c7c6f70b8bef26b0497fa927a5-1737728742647/tgklq5gynipiheuheyci",
     type: "Website",
     stack: "React, Web3.js, Solidity",
     liveURL: "https://nftmarketplace.com",
-    fiverr: "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2EzMDk0N2Q0NjgwMDAxZWMxNzYy",
-    github:"https://github.com/ArmSershant/exodus-project"
+    fiverr:
+      "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2EzMDk0N2Q0NjgwMDAxZWMxNzYy",
+    github: "https://github.com/ArmSershant/exodus-project",
   },
   {
     title: "Uplift Solution",
@@ -26,51 +30,52 @@ export const projects: Project[] = [
     type: "Website",
     stack: "React, GSAP, TailwindCSS, Framer",
     liveURL: "https://upliftsolution.com",
-    fiverr: "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2ExMmY5ZmM3NWIwMDAxNDllZGU5",
-    github:"https://github.com/ArmSershant/uplift-website"
+    fiverr:
+      "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2ExMmY5ZmM3NWIwMDAxNDllZGU5",
+    github: "https://github.com/ArmSershant/uplift-website",
   },
 
   {
     title: "Trilium Quest",
     description:
       "I developed an MMORPG game with Web3 elements (all the items in the game were NFTs and there was a corresponding platform where you could export those items as NFTs and sell them for real money in atomichub and vice versa buy the items from atomichub and use them in the game in)",
-    photoURL:
-      "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/project_item/attachment/75ccc01bd569e609cbe6447d4050efe0-1737727693577/image_original.jpeg",
+    photoURL: triliumGameLogo,
     videoURL:
       "https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/v1/video-attachments/project_item/attachment/75ccc01bd569e609cbe6447d4050efe0-1737727746838/be66zzxwoxwvcxokpnic",
     type: "Game",
     stack: "GDscript, Wax.js, Godot",
     liveURL: "https://trilium-quest.net",
-    fiverr: "https://www.fiverr.com/users/armsershant/portfolio/Njc5MzlmMmNmMDMyYWEwMDAxMTAyYjg1",
-    github:"https://github.com/ArmSershant/godot-game"
+    fiverr:
+      "https://www.fiverr.com/users/armsershant/portfolio/Njc5MzlmMmNmMDMyYWEwMDAxMTAyYjg1",
+    github: "https://github.com/ArmSershant/godot-game",
   },
   {
     title: "Trilium Quest Wiki",
     description:
       "Trilium Quest is an Alien Worlds Community MMORPG game on the Wax blockchain. Thanks to blockchain technology, you can enjoy secure and transparent transactions, as well as true ownership and control over your NFTs and resources. You can connect one of your wallets Anchor or Cloud Wallet to do the importing and exporting of NFTs also for the transactions.",
-    photoURL:
-      "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/project_item/attachment/117357ebcc973fb6c2644da5b4290f56-1737725753938/image_original.jpeg",
+    photoURL: triliumWikiLogo,
     videoURL:
       "https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/v1/video-attachments/project_item/attachment/117357ebcc973fb6c2644da5b4290f56-1737725758217/eocps4v5pqtd4qgyuhek",
     type: "Website",
     stack: "React.js, Wax.js, SCSS",
     liveURL: "trilium-quest.net",
-    fiverr: "https://www.fiverr.com/users/armsershant/portfolio/Njc5Mzk4NDU0N2Q0NjgwMDAxZWMxMmJk",
-    github:"https://github.com/ArmSershant/trilium-project"
+    fiverr:
+      "https://www.fiverr.com/users/armsershant/portfolio/Njc5Mzk4NDU0N2Q0NjgwMDAxZWMxMmJk",
+    github: "https://github.com/ArmSershant/trilium-project",
   },
   {
     title: "Arch Games Studio",
     description:
       "Arch Games, a cutting-edge gaming company, specializes in high-quality games for all platforms. Driven by passion and innovation, we use the latest technology to deliver unforgettable experiences. Our team of graphic designers, blockchain developers, and programming experts employ agile methodologies to create games with stunning visuals, engaging storylines, and seamless blockchain integration. With over 30 years of combined experience, Arch Games leads the industry, dedicated to providing the best possible gaming experiences to players worldwide.",
-    photoURL:
-      "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/project_item/attachment/7c0470cc39096ab77cab09536d28adb3-1737728120012/image_original.jpeg",
+    photoURL: archLogo,
     videoURL:
       "https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/v1/video-attachments/project_item/attachment/ea5e7b83ddd1b28a0a2874242a56bd7f-1737728119837/rmpn18rc77cdlaogemc2",
     type: "Website",
     stack: "React.js, SCSS",
     liveURL: "https://www.archsoftdev.com/",
-    fiverr: "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2EwYTJmMDMyYWEwMDAxMTAyYzJj",
-    github:"https://github.com/ArmSershant/arch-project"
+    fiverr:
+      "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2EwYTJmMDMyYWEwMDAxMTAyYzJj",
+    github: "https://github.com/ArmSershant/arch-project",
   },
   {
     title: "Ticket Metric",
@@ -83,7 +88,8 @@ export const projects: Project[] = [
     type: "Website",
     stack: "React.js, MUI, Apex.js, Chart.js",
     liveURL: "https://ticketmetric.io/",
-    fiverr: "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2EyMmJmMDMyYWEwMDAxMTAyY2Vj",
-    github:"https://github.com/ArmSershant"
+    fiverr:
+      "https://www.fiverr.com/users/armsershant/portfolio/Njc5M2EyMmJmMDMyYWEwMDAxMTAyY2Vj",
+    github: "https://github.com/ArmSershant",
   },
 ];
